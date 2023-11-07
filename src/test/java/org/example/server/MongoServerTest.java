1f299b62b716b4503bac411232d55686e9e46838
Flavio <ffiechter@gmx.ch>
2023-11-07 08:56:42 +0100

    mock find

--- src/test/java/org/example/server/MongoServerTest.java
+++ src/test/java/org/example/server/MongoServerTest.java
@@ -6,74 +6,78 @@ import org.example.model.Supplier;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.mock;
 
 
 class MongoServerTest {
+    private MongoServer mongoServerMock = mock(MongoServer.class);
+    List<Package> packageList = new ArrayList<>();
+    List<Recipient> recipientList = new ArrayList<>();
+    List<Supplier> supplierList = new ArrayList<>();
 
-    private MongoServer mongoServer = new MongoServer();
 
     @BeforeEach
     void setUp() {
+        packageList.add(new Package("test", 1, 1, 1, 1, 2));
+        recipientList.add(new Recipient(3, "test", "test", "test", "test", "test"));
+        supplierList.add(new Supplier(2, "test", "test", "test"));
     }
 
     @Test
     void findPackages() {
-        assertThat(mongoServer.findPackages()).hasSize(4);
+        assertThat(mongoServerMock.findPackages()).hasSize(0);
     }
 
     @Test
     void findRecipients() {
-        assertThat(mongoServer.findRecipients()).hasSize(2);
+        assertThat(mongoServerMock.findRecipients()).hasSize(0);
     }
 
     @Test
     void findSuppliers() {
-        assertThat(mongoServer.findSuppliers()).hasSize(1);
+        assertThat(mongoServerMock.findSuppliers()).hasSize(0);
     }
 
     @Test
     void addPackage() {
-        mongoServer.addPackage(new Package("test", 1, 1, 1, 1, 2));
 
-        assertThat(mongoServer.findPackages()).hasSize(5);
     }
 
     @Test
     void addRecipient() {
-        mongoServer.addRecipient(new Recipient(3, "test", "test", "test", "test", "test"));
 
-        assertThat(mongoServer.findRecipients()).hasSize(3);
     }
 
     @Test
     void addSupplier() {
-        mongoServer.addSupplier(new Supplier(2, "test", "test", "test"));
 
-        assertThat(mongoServer.findSuppliers()).hasSize(2);
     }
 
     @Test
     void editRecipient() {
         Recipient recipientEdit = new Recipient(1, "test", "test", "test", "test", "test");
-        mongoServer.editRecipient(recipientEdit);
-        boolean containsRecipient = mongoServer.findRecipients().contains(recipientEdit);
+        mongoServerMock.editRecipient(recipientEdit);
+        boolean containsRecipient = mongoServerMock.findRecipients().contains(recipientEdit);
         assert containsRecipient : "Recipient not found in the list";
     }
 
     @Test
     void editSupplier() {
         Supplier supplierEdit = new Supplier(1, "test", "test", "test");
-        mongoServer.editSupplier(supplierEdit);
-        boolean containsSupplier = mongoServer.findSuppliers().contains(supplierEdit);
+        mongoServerMock.editSupplier(supplierEdit);
+        boolean containsSupplier = mongoServerMock.findSuppliers().contains(supplierEdit);
         assert containsSupplier : "Supplier not found in the list";
     }
 
     @Test
     void editPackage() {
-        Package aPackageEdit = new Package(1, "test", 1, 1, 1,1,1);
-        mongoServer.editPackage(aPackageEdit);
-        boolean containsPackage = mongoServer.findPackages().contains(aPackageEdit);
+        Package aPackageEdit = new Package(1, "test", 1, 1, 1, 1, 1);
+        mongoServerMock.editPackage(aPackageEdit);
+        boolean containsPackage = mongoServerMock.findPackages().contains(aPackageEdit);
         assert containsPackage : "Package not found in the list";
     }
 }
