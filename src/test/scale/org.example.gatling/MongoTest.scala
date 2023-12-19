54e345e744db830eb8c10aab0b3517b96a0c67c1
laptopAdmin <arunannagarajah@bbzbl-it.ch>
2023-12-19 11:13:24 +0100

    added sekelloten structure for gatling

--- src/test/scale/org.example.gatling/MongoTest.scala
+++ src/test/scale/org.example.gatling/MongoTest.scala
@@ -1,7 +1,7 @@
 import io.gatling.core.Predef._
 import io.gatling.core.scenario.Simulation
 
-class MongoSimulation extends Simulation {
+class MongoSimvulation extends Simulation {
   val scn = scenario("MongoDB Test")
     .exec(session => {
       MongoTest.testMongo() // This is the Java method you need to implement
